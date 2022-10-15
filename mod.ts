b415dbb5e20097ca1af73ad5f57f2bd0dda47e30
hasundue <hasundue@gmail.com>
2022-10-15 11:48:29 +0900

    build(deps): use esm.sh

--- mod.ts
+++ mod.ts
@@ -1,7 +1,7 @@
 import * as semver from "https://deno.land/std@0.159.0/semver/mod.ts";
 import * as commit from "https://deno.land/x/commit@0.1.5/mod.ts";
 import { intersect } from "https://deno.land/std@0.159.0/collections/intersect.ts";
-import { Octokit } from "https://cdn.skypack.dev/@octokit/core@4.0.5?dts";
+import { Octokit } from "https://esm.sh/@octokit/core@4.0.5";
 
 const octokit = new Octokit({ auth: Deno.env.get("GITHUB_TOKEN") });
 
