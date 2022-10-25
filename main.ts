9bc8b033ef15d2497caecdfc3dac66705ef06bf2
hasundue <hasundue@gmail.com>
2022-10-25 18:06:59 +0900

    fix(main): use CommandBuilder instead of $

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import { Command } from "https://deno.land/x/cliffy@v0.25.2/command/command.ts";
 import { getDefaultChangelog } from "https://deno.land/x/ghlog@0.3.4/mod.ts";
 import { getUpdates } from "https://deno.land/x/denopendabot@0.7.0/mod.ts";
-import $ from "https://deno.land/x/dax@0.15.0/mod.ts";
+import { CommandBuilder } from "https://deno.land/x/dax@0.15.0/mod.ts";
 import { Octokit } from "https://esm.sh/@octokit/core@4.1.0";
 import { getNewVersion } from "./mod.ts";
 
@@ -41,7 +41,7 @@ if (!tag) {
 }
 
 if (Deno.env.get("CI")) {
-  await $`echo "VERSION=${tag}" >> $GITHUB_OUTPUT`;
+  await new CommandBuilder().command(`echo "VERSION=${tag}" >> $GITHUB_OUTPUT`);
 }
 
 // check if dependencies are up to date
