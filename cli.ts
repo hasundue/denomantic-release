d08ca2c33b5b42bfd2ee6fffc491f2417c2150ee
denopendabot <denopendabot@github.com>
2022-12-06 00:20:46 +0000

    build(deps): bump denopendabot from 0.13.0 to 0.14.0

--- cli.ts
+++ cli.ts
@@ -1,6 +1,6 @@
-import { Command } from "https://deno.land/x/cliffy@v0.25.5/command/command.ts";
+import { Command } from "https://deno.land/x/cliffy@v0.25.4/command/command.ts";
 import { getDefaultChangelog } from "https://deno.land/x/ghlog@0.3.4/mod.ts";
-import { getUpdates } from "https://deno.land/x/denopendabot@0.13.0/mod.ts";
+import { getUpdates } from "https://deno.land/x/denopendabot@0.14.0/mod.ts";
 import { Octokit } from "https://esm.sh/@octokit/core@4.1.0";
 import { getNewVersion } from "./mod.ts";
 
